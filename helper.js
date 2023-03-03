61bea8ec4d8af26ea42fd899876cc39ae1dcc68c
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-03-03 12:58:07 -0300

    Add files via upload

---
+++ helper.js
@@ -0,0 +1,5 @@
+let result;
+for(i=1;i<=80;i++){
+    result+='<img id="frequencyIMG'+i+'">'
+}
+console.log(result)
--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-
-
-
-
 function main(){
     let input = document.getElementById("input").value;
-    for(l = 1; l <= 32; l++){
-        document.getElementById("img"+l).style.display = "none";
+    for(l = 1; l <= 80; l++){
+
+        //hide binary numbers
+        document.getElementById("binary"+l).style.display = "none";
+
+        //hide binary images
+        document.getElementById("binaryIMG"+l).style.display = "none";
+
+        //hide amplitude modulation
+        document.getElementById("amplitudeIMG"+l).style.display = "none";
+
+        //hide frequency modulation
+        document.getElementById("frequencyIMG"+l).style.display = "none";
     }
-    console.log(toBinary(input));
+
     printImages(toBinary(input));
 }
 
@@ -16,20 +23,80 @@ function toBinary(input){
     for(i = 0; i < input.length; i++){
         output.push(input[i].charCodeAt(0).toString(2).padStart(8, "0"));
     }
-    return (output);
+    return output;
+}
+
+function testNeighbors(position, array, counter){
+    let string = "";
+    let returnValue = 0;
+
+    for(let l = 0; l < array.length; l++){
+        string += array[l]
+    }
+
+    let newArray = string.split('');
+    //console.log(string)
+    //console.log(newArray)
+    //console.log("counter="+counter)
+    let currentPosition = newArray[counter-1]
+    //console.log("currentposition="+currentPosition)
+
+    //testing the current position for neighbors
+
+
+    //from 0 to 1 to 0
+    if(currentPosition == "1" && newArray[counter-2] == "0" && newArray[counter] == "0"){
+        returnValue = 4;
+    }
+
+    //from 0 to 1
+    else if(currentPosition == "1" && newArray[counter-2] == "0"){
+        returnValue = 2;
+    }
+
+    //from 1 to 0
+    else if(currentPosition == "1" && newArray[counter] == "0"){
+        returnValue = 3;
+    }
+
+    //from 1 to 1
+    else if(currentPosition == "1" && newArray[counter-2] == "1" || currentPosition == "1" && newArray[counter] == "1"){
+        returnValue = 1;
+    }
+
+    //if 0
+    else{
+        returnValue = 0;
+    }
+
+    return returnValue;
 }
 
 function printImages(input){
-    console.log(input.length)
     let counter = 0;
     for(i = 0; i < input.length; i++){
-        temp = input[i];
-        //console.log(input[i]);
+        let temp = input[i];
         for(j = 0; j < temp.length; j++){
-            console.log(temp[j]);
             counter++;
-            document.getElementById("img"+counter).style.display = "inline"; 
-            document.getElementById("img"+counter).src="temp"+temp[j]+".png";
+
+            let neighbors = parseInt(testNeighbors(temp[j], input, counter));
+            console.log("neighbors="+neighbors)
+
+            //show binary numbers
+            document.getElementById("binary"+counter).style.display = "inline"; 
+            document.getElementById("binary"+counter).src="assets/binary"+temp[j]+".png";
+
+            //show binary images (need neighbors)
+            document.getElementById("binaryIMG"+counter).style.display = "inline"; 
+            document.getElementById("binaryIMG"+counter).src="assets/binaryIMG"+neighbors+".png";
+
+            //show amplitude modulation
+            document.getElementById("amplitudeIMG"+counter).style.display = "inline"; 
+            document.getElementById("amplitudeIMG"+counter).src="assets/amplitudeIMG"+temp[j]+".png";
+
+            //show frequency modulation
+            document.getElementById("frequencyIMG"+counter).style.display = "inline"; 
+            document.getElementById("frequencyIMG"+counter).src="assets/frequencyIMG"+temp[j]+".png";
         }
     }
 }

10e20d31e71d5a1c04badf0dfb784356bb433fed
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-03-03 14:24:01 -0300

    Add files via upload

--- helper.js
+++ helper.js
@@ -1,3 +1,5 @@
+//como o nome sugere, esse arquivo serve pra me ajudar a escrever bastante coisa sem precisar escrever bastante coisa
+
 let result;
 for(i=1;i<=80;i++){
     result+='<img id="frequencyIMG'+i+'">'
--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+//funcao chamada sempre que o input do HTML muda
 function main(){
+    //pega o texto que o usuario colocou no input
     let input = document.getElementById("input").value;
+    
+    //esse loop limpa as imagesns da tela, o numero 80 ali significa a quantidade maxima de caracteres binarios, ou seja, o numero maximo de "partes de grafico" possiveis em cada categoria
     for(l = 1; l <= 80; l++){
 
         //hide binary numbers
@@ -15,17 +19,25 @@ function main(){
         document.getElementById("frequencyIMG"+l).style.display = "none";
     }
 
+    //chama a funcao que mostra as imagens, e envia para ela o input do usuario em forma de valor binario
     printImages(toBinary(input));
 }
 
+//funcao que transforma um input alfanumerico em um output binario
 function toBinary(input){
+
+    //cria uma array para organizar o output
     let output = [];
+
+    //loop para transformar cada caractere em binario
     for(i = 0; i < input.length; i++){
+        //o .push envia o binario para a array de output, o charCodeAt(0) transforma de alfanumerico para o valor ASCII, o .toString(2) transforma de ASCII em binario e o .padStart(8, "0") certifica que o binario tenha base 8, ele adiciona o numero "0" no inicio do codigo binario CASO o codigo tenha MENOS de 8 caracteres 
         output.push(input[i].charCodeAt(0).toString(2).padStart(8, "0"));
     }
     return output;
 }
 
+//funcao que testa os vizinhos de um byte, necessario para mostrar as imagens corretas no sinal binario
 function testNeighbors(position, array, counter){
     let string = "";
     let returnValue = 0;
@@ -35,14 +47,11 @@ function testNeighbors(position, array, counter){
     }
 
     let newArray = string.split('');
-    //console.log(string)
-    //console.log(newArray)
-    //console.log("counter="+counter)
+    
     let currentPosition = newArray[counter-1]
-    //console.log("currentposition="+currentPosition)
 
-    //testing the current position for neighbors
-
+    
+    //testa a posicao atual e seus vizinhos
 
     //from 0 to 1 to 0
     if(currentPosition == "1" && newArray[counter-2] == "0" && newArray[counter] == "0"){
@@ -72,6 +81,7 @@ function testNeighbors(position, array, counter){
     return returnValue;
 }
 
+//funcao que mostra as imagens
 function printImages(input){
     let counter = 0;
     for(i = 0; i < input.length; i++){
@@ -79,7 +89,8 @@ function printImages(input){
         for(j = 0; j < temp.length; j++){
             counter++;
 
-            let neighbors = parseInt(testNeighbors(temp[j], input, counter));
+            
+            let neighbors = testNeighbors(temp[j], input, counter);
             console.log("neighbors="+neighbors)
 
             //show binary numbers
